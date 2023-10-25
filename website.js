145ebac3042a37ea54f91a16a9ef9de97e9687a4
Pshubham241 <118792296+Pshubham241@users.noreply.github.com>
2023-10-26 01:39:59 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,12 @@
+function myfun()
+{
+    let menubar = document.getElementById("menu")
+    if(menubar.style.display == "block")
+    {
+        menubar.style.display ="none";
+    }
+    else 
+    {
+        menubar.style.display = "block";
+    }
+}
